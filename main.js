5c2069f57edca6cfef0cbba95498979041523334
evanthoven95 <evanthoven95@gmail.com>
2025-05-23 12:45:55 -0600

    Agregado event listener en radio buttons, ahora se pueden usar dos dados

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const button= document.getElementById("boton");
-const dado=document.getElementById("dado");
+const resultados= document.getElementById("resultados")
+// const dado=document.getElementById("dado");
 
 button.addEventListener('click',function(){
-    const numero=Math.floor(Math.random()*6)+1;
-    let ruta="";
-    switch(numero){
+    resultados.innerHTML=""; //limpieza de imagenes anteriores
+
+    const selectDado= document.querySelector('input[name="dados"]:checked'); //Para saber que dado ha sido seleccionado
+    const cantidadDados= parseInt(selectDado.value); //se obtiene el value de los radio buttons y se convierten 
+    // a enteros para poder ser iterados
+
+    for(let i=0; i<cantidadDados;i++){ //se itera i para que genere un numero aleatorio por cada dado, en este caso hay dos, indices 0 y 1.
+        const numero=Math.floor(Math.random()*6)+1;
+        let ruta="";
+         
+        
+        switch(numero){
         case 1:
             ruta="./dado-1.png"
             break;
@@ -24,8 +34,26 @@ button.addEventListener('click',function(){
             ruta="./dado-6.png"
             break;
     }
+    
+    
+    const img = document.createElement("img"); //crea un elemento imagen
+    img.src = ruta; //se agregan las rutas de acuerdo al case que corresponda
+    img.alt = `Dado ${numero}`; 
+    img.style.width = "150px";
+    img.style.marginRight = "50px";
 
-    dado.src=ruta;
-    dado.style.display="block";
+    resultados.appendChild(img);
+   
+    }
+    
 });
 
+
+const radios= document.querySelectorAll('input[name="dados"]'); //Para saber que dado ha sido seleccionado
+radios.forEach(radio=>{
+    radio.addEventListener("change", function(){
+        if (radio.checked){
+            console.log(`Seleccionaste ${radio.value} dado`);
+        }
+    });
+});
